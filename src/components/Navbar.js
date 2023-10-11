e03c5b21c074500286f99b15aba9f7484650974a
YashPimpalkar <62347457+YashPimpalkar@users.noreply.github.com>
2023-10-11 19:10:15 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,31 +4,28 @@ import {
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link, useNavigate } from 'react-router-dom';
-import Logo from '../assets/images/Logo.png'
+import Logo from '../assets/images/Logo.png';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
 
-
-
-
-
 function ResponsiveAppBar() {
 
+  const user = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate();
 
-
-  const user=JSON.parse(localStorage.getItem('user'));
-  const navigate =useNavigate();
-  const handleLogout =async ()=>{
-      try {
-          await signOut(auth);
-          localStorage.removeItem('token');
-          localStorage.removeItem('user');
-          navigate("login");
-      } catch (error) {
-          console.error(error)
-      }
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      navigate("login");
+    } catch (error) {
+      console.error(error)
+    }
   }
+
   const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -38,19 +35,28 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <AppBar position="static">
+
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleOpenNavMenu}>
+        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleOpenNavMenu}  >
           <MenuIcon />
         </IconButton>
         <Menu
           id="menu-appbar"
           anchorEl={anchorElNav}
           anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
           keepMounted
           transformOrigin={{
             vertical: 'top',
@@ -59,76 +65,102 @@ function ResponsiveAppBar() {
           open={Boolean(anchorElNav)}
           onClose={handleCloseNavMenu}
         >
-          <MenuItem>
-          <Box 
-          component={Link} 
-          to="/" 
-          sx={{
-            textDecoration: 'none', 
-            color: 'inherit',
-            '&:hover': {
-              color: 'red',
-            }
-          }}
-        >
-          Home
-        </Box>
+          {/* ... other menu items ... */}
+        
+          <MenuItem >
+            
+              <Box
+                component={Link}
+                to="/"
+                sx={{
+                  textDecoration: 'none',
+                  color: 'inherit',
+                  '&:hover': {
+                    color: 'red',
+                  }
+                }}
+              >
+                Home
+              </Box>
           </MenuItem>
           <MenuItem>
-          <Box 
-          component="a" 
-          href="#exercises" 
-          sx={{
-            textDecoration: 'none', 
-            color: 'inherit',
-            '&:hover': {
-              color: 'red',
-            }
-          }}
-        >
-          Exercises
-        </Box>
+            <Box
+              component="a"
+              href="#exercises"
+              sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+                '&:hover': {
+                  color: 'red',
+                }
+              }}
+            >
+              Exercises
+            </Box>
           </MenuItem>
           <MenuItem>
-          <Box 
-          component={Link} 
-          to="/pricing" 
-          sx={{
-            textDecoration: 'none', 
-            color: 'inherit',
-            '&:hover': {
-              color: 'red',
-            }
-          }}
-        >
-          Pricing
-        </Box>
-          </MenuItem>
-          <MenuItem>
-            {user && user.email}
-          </MenuItem>
-          <MenuItem>
-          <Avatar 
-            src={user.photoURL} alt="profile" />
-          </MenuItem>
-          <MenuItem>
-            <button onClick={handleLogout} className="logout-btn">LogOut</button>
-          </MenuItem>
-        </Menu>
+            <Box
+              component={Link}
+              to="/pricing"
+              sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+                '&:hover': {
+                  color: 'red',
+                }
+              }}
+            >
+              Pricing
+            </Box>
+          
+        </MenuItem>
+       
+      </Menu>
 
-        {/* This is your logo from the Stack component */}
-        <Link to="/">
-          <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 0 0px 0px' }} />
-        </Link>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-           Fitness-Club
-        </Typography>
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0 0 0px 0px' }} />
+      </Link>
+      <Typography variant="h6" style={{ flexGrow: 1 }}>
+        Fitness-Club
+      </Typography>
 
-        {/* Here you can continue with other parts of the AppBar, like user icons, etc. */}
-        <Avatar  src={user.photoURL} alt="profile"  />
+      <Tooltip title="User Account">
+        <IconButton onClick={handleOpenUserMenu} color="inherit">
+          <Avatar src={user.photoURL} alt="profile" />
+        </IconButton>
+      </Tooltip>
 
-      </Toolbar>
-    </AppBar>
+      <Menu
+        id="menu-user"
+        anchorEl={anchorElUser}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        <MenuItem>
+          {user && user.email}
+        </MenuItem>
+        <MenuItem>
+          <Box
+            component={Button}
+            onClick={handleLogout}
+            className="logout-btn"
+            sx={{
+              textDecoration: 'none',
+              color: 'inherit',
+              '&:hover': {
+                color: 'red',
+              }
+            }}
+          >
+            Logout
+          </Box>
+        </MenuItem>
+      </Menu>
+    </Toolbar>
+    </AppBar >
   );
 }
 
